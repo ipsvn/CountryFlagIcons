4ed669d724ee30cc1e9bb0f5664ee16d9bbb8e0d
svn <ipsvn@protonmail.com>
2025-08-05 20:43:29 +0100

    updated sig

--- Memory.cs
+++ Memory.cs
@@ -6,5 +6,5 @@ namespace CountryFlagIcons;
 public static class Memory
 {
     public static readonly MemoryFunctionVoid<CCSPlayerController, IntPtr, IntPtr, IntPtr> CCSPlayerController_InventoryUpdateThink = 
-        new("48 8B BF 40 0A 00 00 E9");
+        new("48 8B BF E8 0A 00 00 E9");
 }

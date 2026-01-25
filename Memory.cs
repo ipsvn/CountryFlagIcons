58d88636ebb422286bc473c61c5fd437160b8d24
bespredel1337 <124391377+bespredel1337@users.noreply.github.com>
2026-01-25 13:11:03 +0300

    Linux signature update

--- Memory.cs
+++ Memory.cs
@@ -7,5 +7,5 @@ namespace CountryFlagIcons;
 public static class Memory
 {
     public static readonly MemoryFunctionVoid<CCSPlayerController_InventoryServices> InventoryUpdateThink = 
-        new(RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "55 48 89 E5 41 57 41 56 41 55 49 89 FD 41 54 53 48 81 EC ? ? ? ? E8 ? ? ? ? 4C 89 EF" : "40 55 53 56 57 41 54 41 55 41 56 41 57 48 8B EC 48 83 EC ? 48 8B 41");
+        new(RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "55 48 89 E5 41 57 49 89 FF 41 56 41 55 41 54 53 48 81 EC ? ? ? ? E8 ? ? ? ? 4C 89 FF" : "40 55 53 56 57 41 54 41 55 41 56 41 57 48 8B EC 48 83 EC ? 48 8B 41");
 }
